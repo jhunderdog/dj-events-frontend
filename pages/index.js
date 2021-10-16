d12a8074621b32cce000910a7412f5f480bc731c
jhunderdog <33382water@gmail.com>
2021-10-16 11:41:22 +0900

    getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
 import Head from 'next/head'
 import Link from 'next/link';
-export default function HomePage() {
+export default function HomePage({events}) {
+  console.log(events);
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   )
+}
+
+export async function getStaticProps(){
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json()
+  console.log(events);
+  return {
+    props: {events},
+    revalidate: 1,
+  }
 }
