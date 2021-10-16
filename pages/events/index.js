4eb70e373ecd7bafabae2fe97cff6e848bc81793
jhunderdog <33382water@gmail.com>
2021-10-16 12:01:06 +0900

    displaying events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,27 @@
-import Layout from "@/components/Layout"
-export default function EventsPage() {
-    return (
-        <Layout>
-            <h1>My Events</h1>
-        </Layout>
-    )
+import Layout from '@/components/Layout';
+import EventItem from '@/components/EventItem';
+import { API_URL } from '@/config/index';
+import Head from 'next/head'
+import Link from 'next/link';
+export default function EventsPage({events}) {
+  console.log(events);
+  return (
+    <Layout>
+      <h1>Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+      {events.map(evt => (
+        <EventItem key={evt.id} evt={evt}/>
+      ))}
+    </Layout>
+  )
+}
+
+export async function getStaticProps(){
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json()
+  console.log(events);
+  return {
+    props: {events},
+    revalidate: 1,
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Layout from '@/components/Layout';
+import EventItem from '@/components/EventItem';
 import { API_URL } from '@/config/index';
 import Head from 'next/head'
 import Link from 'next/link';
@@ -7,6 +8,13 @@ export default function HomePage({events}) {
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+      {events.map(evt => (
+        <EventItem key={evt.id} evt={evt}/>
+      ))}
+      {events.length > 0 && (<Link href='/events'>
+        <a className='btn-secondary'>View All Events</a>
+      </Link>)}
     </Layout>
   )
 }
@@ -16,7 +24,7 @@ export async function getStaticProps(){
   const events = await res.json()
   console.log(events);
   return {
-    props: {events},
+    props: {events: events.slice(0, 3)},
     revalidate: 1,
   }
 }
