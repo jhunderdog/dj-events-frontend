4c8837e8c1ea78b604906d40d149df2b8f13c23a
jhunderdog <33382water@gmail.com>
2021-10-20 14:08:27 +0900

    Events Functionality

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,7 @@
 import Link from 'next/link';
+import Search from './Search';
 import styles from '@/styles/Header.module.css';
+
 
 export default function Header() {
     return (
@@ -9,11 +11,17 @@ export default function Header() {
                     <a>DJ Events</a>
                 </Link>
             </div>
+            <Search/>
             <nav>
                 <ul>
                     <li>
                         <Link href='/events'>
                             <a>Events</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href='/events/add'>
+                            <a>Add Event</a>
                         </Link>
                     </li>
                 </ul>
